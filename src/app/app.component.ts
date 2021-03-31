cbb5e378f1c9146a0e702f8a1e61da15ab8961e5
Matt Hippely <mhippely@vmware.com>
2021-03-31 13:21:20 -0700

    default server driven datagrid
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { ClrDatagridStateInterface } from '@clr/angular';
+import { FetchResult, Inventory } from './utils/inventory';
+import { User } from './utils/user';
 
 @Component({
   selector: 'app-root',
+  providers: [Inventory],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'gh5621';
+  users: User[];
+  total: number;
+  loading = true;
+
+  constructor(private inventory: Inventory) {
+    inventory.size = 103;
+    this.inventory.latency = 500;
+    inventory.reset();
+  }
+
+  refresh(state: ClrDatagridStateInterface<User>) {
+    this.loading = true;
+    const filters: { [prop: string]: any[] } = {};
+    if (state.filters) {
+      for (const filter of state.filters) {
+        const { property, value } = filter;
+        filters[property] = [value];
+      }
+    }
+    this.inventory
+      .filter(filters)
+      .sort(state.sort as { by: string; reverse: boolean })
+      .fetch(state.page.size * (state.page.current - 1), state.page.size)
+      .then((result: FetchResult) => {
+        this.users = result.users;
+        this.total = result.length;
+        this.loading = false;
+      });
+  }
 }
